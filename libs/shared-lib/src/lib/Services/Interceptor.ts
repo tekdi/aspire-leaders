26b6b14167b7d5b49379a7e7d61e861e8cf05471
suvarnakale <suvarna.kale@tekditechnologies.com>
2025-05-12 14:49:10 +0530

    Interceptor and added tenant in header

--- libs/shared-lib/src/lib/Services/Interceptor.ts
+++ libs/shared-lib/src/lib/Services/Interceptor.ts
@@ -36,8 +36,8 @@ instance.interceptors.request.use(
       }
     }
     // config.headers.tenantid = '4783a636-1191-487a-8b09-55eca51b5036';
-    // config.headers.tenantid = 'fbe108db-e236-48a7-8230-80d34c370800';
-    config.headers.tenantid = localStorage.getItem('tenantId') || "";
+    config.headers.tenantId = 'ef99949b-7f3a-4a5f-806a-e67e683e38f3';
+    // config.headers.tenantid = localStorage.getItem('tenantId') || "";
     return config;
   },
   (error) => {
